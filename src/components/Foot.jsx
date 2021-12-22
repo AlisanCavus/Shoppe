67886ec5cdf904f1e1b0c215b492f855e9568361
AlisanCavus <cavusalisan@gmail.com>
2021-12-22 16:14:37 +0100

    hello mf

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -1,10 +1,14 @@
 import {  FaGithub } from 'react-icons/fa'
+
 function Foot() {
+
     return (
+       
         <div className="foot">
             <div className="txt-foot">Copyright&copy;2022</div>
             <a href="https://github.com/AlisanCavus" ><FaGithub> </FaGithub></a>          
         </div>
+    
     )
 }
 
--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,8 +1,11 @@
 import Logo from '../assets/logo.svg'
 
+
 function Head() {
+    
+    
     return (
-        <div className="head ">
+        <div className="head" >
             <div className="headlogo"> <img className="animate__backInLeft" src={Logo} alt="logo"/> Shoppe</div>
             <div className="txt">Your daily shopping list.</div>
         </div>
