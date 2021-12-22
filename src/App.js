c50a195b83a1afc06e637cd541e5375a310326f4
AlisanCavus <cavusalisan@gmail.com>
2021-12-22 11:16:57 +0100

    again

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { Header } from "./Components/styled/Header.styled";
-import { Footer } from "./Components/styled/Footer.styled";
-import H1 from "./Components/styled/H1.styled";
-import H4 from "./Components/styled/H4.styled";
-import Container from "./Components/styled/Container.styled";
-import FootH4 from './Components/styled/H4.styled';
-
 function App() {
   return (
     <div>
-      <Header>
-        <H1>Shoppe</H1>
-        <H4>Your daily shopping list.</H4>
-      </Header>
-      <Container>
-
-      </Container>
-      <Footer>
-        <div>
-        <Footer FootH4>Shoppe&copy;Copyright 2022</Footer>
-        </div>
-      </Footer>
+  
     </div>
   );
 }
