bf85de45f4883551b8403018cf6d523e6967cc70
AlisanCavus <cavusalisan@gmail.com>
2021-12-23 16:19:15 +0100

    skkdlas

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -3,18 +3,27 @@ import { useSpring, animated } from 'react-spring';
 
 function AddItem() {
 
-    const sty = useSpring({
-        from: { opacity: 0, },
-        to: { opacity: 1, },
-        delay: 100,
-      })
-    return (
-        <animated.div style={ sty }>
-            <form className="form">
-                <input className="input" type="text" placeholder="Add an Item"/>
-            </form>
-        </animated.div>
-    )
+  
+  
+
+  const sty = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 100,
+  });
+  
+  return (
+    <animated.div style={sty}>
+      <form className="form">
+        <input
+          className="input"
+          type="text"
+          placeholder="Add an Item"
+        />
+      </form>
+    </animated.div>
+  )
+
 }
 
 export default AddItem
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,27 +1,52 @@
 import { FiPlusSquare } from 'react-icons/fi';
 import { FiMinusSquare } from 'react-icons/fi';
 import { FiTrash2 } from 'react-icons/fi';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import ListItemsTemplate from '../../ListItemsTemplate';
 
 function Card() {
   const [shoplist, setShopList] = useState(ListItemsTemplate);
+  
+  useEffect(() => {
+    localStorage.setItem('shops', JSON.stringify(shoplist))
+  }, [shoplist])
 
   if (!ListItemsTemplate || ListItemsTemplate.length === 0) {
-    return <p>You don't have any shopping list.</p>;
+    return <p style={{textAlign: 'center'}}>You don't have any shopping list.</p>;
   }
+
+  
+
+  const handlePlus = (index) => {
+      const newShopItems = [...shoplist]
+      newShopItems[index].amount ++
+      setShopList(newShopItems)
+  }
+
+  const handleMinus = (index) => {
+    const newShopItems = [...shoplist]
+    newShopItems[index].amount --
+    setShopList(newShopItems)
+    }
+
+    const handleDelete = (index) => {
+        const newShopItems = [...shoplist]
+        newShopItems.splice(index, 1)
+        setShopList(newShopItems)
+    }
+
 
   return (
     <div>
-      {shoplist.map((id) => (
+      {shoplist.map((id,index) => (
         <div key={id.id}>
           <div   className="card">
             <p>{id.item}</p>
             <div className="card2">
-              <FiPlusSquare className="pad" />
+              <FiPlusSquare className="pad" onClick={() => handlePlus(index)} />
               <div className="pad">{id.amount}</div>
-              <FiMinusSquare className="pad" />
-              <FiTrash2 className="pad" />
+              <FiMinusSquare className="pad" onClick={() => handleMinus(index)} />
+              <FiTrash2 className="pad" onClick={() => handleDelete(index)} />
             </div>
           </div>
         </div>
